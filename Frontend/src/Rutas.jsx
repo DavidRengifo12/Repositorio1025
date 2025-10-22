12f777cdfe4a3f0694b2915b8f6d6bd84143ef96
DavidRengifo12 <davidsantiagorengifoguacheta@gmail.com>
2025-10-21 23:07:05 -0500

    Repo

---
+++ Frontend/src/Rutas.jsx
@@ -0,0 +1,61 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+
+
+import ToastProvider from "./components/ToastProvider";
+
+import LandingPage from "./LandingPage";
+import Login from "./Components/Login";
+import Register from "./Components/Register";
+
+
+
+
+
+const Rutas =() => { //PROTEGER LAS RUTAS CON AUTHCONTEXT
+  return (
+    <>
+    
+     <BrowserRouter>
+        <ToastProvider />
+        <Routes>
+           <Route path="/" element={<LandingPage />} />
+           <Route path="/login" element={<Login />} />
+           <Route path="/register" element={<Register />}/>
+        </Routes>
+        {/* <RutasWeb /> */}
+     </BrowserRouter> 
+     
+    </>
+  )
+}
+
+// const RutasWeb = () => {
+  
+//   return(
+//     <Routes>
+//       <Route path="/" element={<LandingPage />} />
+//       <Route path="login" element={<Login />} />
+//       <Route path="register" element={<Register />} />
+
+//       {/*Rutas Administrador */}
+//       <Route path="/dash-admin" element={isAuthenticated && user.rol === "administrador" ? <AdminLayout />: <Navigate to= '/' />}>
+//          {/* <Route index element={<Navigate to="equipo-computo" replace />} />
+//          <Route path="equipo-computo" element={<GestionEquipos />} />
+//          <Route path="chat" element={<ChatAdmin />} />
+//         <Route path="equipos-reservados" element={} /> */}
+//       </Route>
+
+//       {/*Rutas Usuario */}
+//       <Route path="/dash-user" element={isAuthenticated && user.rol === 'usuario' ? <NavbarR /> : <Navigate to='/' />}>
+//         {/* <Route index element={<Navigate to="reserva-equipo" replace />} />
+//         <Route path="reserva-equipo" element={<ReservaEquipo />} />
+//         <Route path="mis-reservas" element={<MisReservas />} />
+//         <Route path="chat" element={<ChatUsuario />} /> */}
+//       </Route>
+
+
+//     </Routes>
+//   )
+// }
+
+export default Rutas
